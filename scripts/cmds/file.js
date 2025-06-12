355d5b0421ac648f0c7a508c9269ce2e7773e918
BOTV2FAHAD <md.fahad.226262@gmail.com>
2025-06-12 14:51:33 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61555745371339"];
+    const permission = ["61576394145332"];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("⩸__ 🦆𝙰𝚛𝚎 𝚢𝚘𝚞 𝚌𝚛𝚊𝚣𝚢 𝙾𝚗𝚕𝚢 𝙵𝙰𝙷𝙰𝙳 𝙱𝚘𝚜𝚜𝚎 𝚙𝚎𝚛𝚖𝚒𝚜𝚜𝚒𝚘𝚗 𝚝𝚘 𝚞𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝙵𝚒𝚕𝚎. ⚠️", event.threadID, event.messageID);
     }
